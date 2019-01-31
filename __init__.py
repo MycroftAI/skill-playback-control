0c0e330065d0a22398340d941916d0d70ed15dea
Steve Penrod <steve.penrod@mycroft.ai>
2019-01-30 20:25:20 -0600

    Switch from play to play_dialog()

--- __init__.py
+++ __init__.py
@@ -62,7 +62,7 @@ class PlaybackControlSkill(MycroftSkill):
 
     @intent_handler(IntentBuilder('').require('Play').require('Phrase'))
     def play(self, message):
-        self.speak("just.one.moment")
+        self.speak_dialog("just.one.moment")
 
         # Remove everything up to and including "Play"
         # NOTE: This requires a Play.voc which holds any synomyms for 'Play'
